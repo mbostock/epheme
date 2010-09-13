85aae750216ea48f5edc4c1c72d57d16196d43b4
Mike Bostock <mbostock+gitorious@gmail.com>
2010-09-12 21:02:06 -0700

    Tweak interpolate logic. Tweak dot example.

--- src/interpolate.js
+++ src/interpolate.js
@@ -2,30 +2,34 @@ eo.interpolate = function(a, b) {
   if (typeof a != "number" || typeof b != "number") {
     var u = eo_interpolate_digits.exec(a);
     a = parseFloat(a);
-    b = parseFloat(b);
+    b = parseFloat(b) - a;
     if (u) {
       u = u[1];
       return function() {
-        var t = eo.time;
-        return a * (1 - t) + b * t + u;
+        return a + b * eo.time + u;
       };
     }
   }
+  b -= a;
   return function() {
-    var t = eo.time;
-    return a * (1 - t) + b * t;
+    return a + b * eo.time;
   };
 };
 
 eo.interpolateRgb = function(a, b) {
   a = eo.rgb(a);
   b = eo.rgb(b);
+  var ar = a.r,
+      ag = a.g,
+      ab = a.b,
+      br = b.r - ar,
+      bg = b.g - ag,
+      bb = b.b - ab;
   return function() {
-    var t = eo.time,
-        q = 1 - t;
-    return "rgb(" + Math.round(a.r * q + b.r * t)
-        + "," + Math.round(a.g * q + b.g * t)
-        + "," + Math.round(a.b * q + b.b * t)
+    var t = eo.time;
+    return "rgb(" + Math.round(ar + br * t)
+        + "," + Math.round(ag + bg * t)
+        + "," + Math.round(ab + bb * t)
         + ")";
   };
 };
